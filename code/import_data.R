6596ea43dcc633013f7727550ce76729a21dbc2a
JoseRafael97 <jose.rafael.feitosa@gmail.com>
2019-06-19 09:50:07 -0300

    update varibles and csv

--- code/import_data.R
+++ code/import_data.R
@@ -3,19 +3,21 @@ library(jsonlite) # Ler dados json
 library(dplyr) # 
 library(here)
 library(tidyr)
+library(readr)
 
-avaliacoes <- fromJSON("http://localhost:5000/api/v1/evaluation/Esperan%C3%A7a", flatten = TRUE)[[1]][[1]]
 
-d = avaliacoes %>% 
-    unnest(itens)    
+avaliacoes <- fromJSON("http://localhost:5000/api/v1/evaluation/Esperan%C3%A7a", flatten = TRUE)
 
-avaliacoes = avaliacoes %>% 
-    group_by(item = item_abreviado, municipio, criterio) %>% 
+avaliacoes <- avaliacoes %>% 
+    unnest(criterions) %>%
+    unnest(itens) %>%
+    group_by(item = name1, municipio = county, criterio = name) %>% 
     summarise(
-        encontrado_turmalina = encontrado, 
-        local_encontrado_turmalina = onde_foi_encontrado
-        )
-
+        valid, 
+        found,
+        pathSought
+    )
+    
 
 avaliacoes %>% 
     write_csv(here::here("data/resultados_avaliacoes.csv"))
