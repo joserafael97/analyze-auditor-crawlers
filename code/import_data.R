8ac3553b35e35bb73c7613480fbe7e4bc04f9374
joserafael97 <jose.rafael.feitosa@gmail.com>
2019-10-09 17:15:00 -0300

    create suport a multiple evaluation analysis

--- code/import_data.R
+++ code/import_data.R
@@ -4,9 +4,12 @@ library(dplyr) #
 library(here)
 library(tidyr)
 library(readr)
+library(lubridate)
 
+avaliacoes_dataset <- readr::read_csv(here::here("data/resultados_avaliacoes.csv"))
+    
+avaliacoes <- data.frame(fromJSON("http://localhost:5000/api/v1/evaluation/bandit/last", flatten = TRUE))
 
-avaliacoes <- data.frame(fromJSON("http://localhost:5000/api/v1/evaluation/bfs/last", flatten = TRUE))
 avaliacoes$duration <- NULL
 avaliacoes$durationMin <- NULL
 
@@ -19,15 +22,21 @@ avaliacoes$`_id` <- NULL
 avaliacoes <- avaliacoes %>% 
     rename(criterio = name) %>% 
     unnest(itens) %>%
-    group_by(item = name, municipio = county, criterio) %>% 
+    group_by(item = name, municipio = county, criterio, aproach) %>% 
     summarise(
-        valid, 
+        valid,
+        contNodeNumberAccess,
         found,
+        date = as.POSIXct(gsub('Z', ' ', gsub('T', ' ', date))),
         pathSought,
-        durationMin,
-        duration
+        durationMin = as.numeric(durationMin),
+        duration = as.numeric(duration)
     )
-    
+
+
+avaliacoes<-full_join(avaliacoes, avaliacoes_dataset, by=c("municipio", "item", "criterio", "aproach", 
+                                                           "date", "valid", "contNodeNumberAccess",
+                                                           "found", "pathSought", "durationMin", "duration"))
 
 avaliacoes %>% 
     write_csv(here::here("data/resultados_avaliacoes.csv"))
